e2fe6c38b262fd8fa6e2d9b8ab82925c1dfcd1d6
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-07-19 03:12:34 -0700

    Update prst6.c

--- cs50/Week 1/prst6.c
+++ cs50/Week 1/prst6.c
@@ -1,5 +1,10 @@
+
 #include <stdio.h>
 #include <cs50.h>
+
+bool check_valid(long long );
+int find_length(long long );
+bool checksum(long long );
 
 int main(void)
 {
@@ -20,5 +25,29 @@ bool check_valid(long long number_card)
 {
     int length = find_length(number_card);
 
-    if (length == 13 || length == 15 || length == 16)
+    return (length == 13 || length == 15 || length == 16) && checksum(number_card);
 }
+
+int find_length(long long number)
+{
+    int len;
+    for (len = 0; number !=0; number /=10, len++);
+    return len;
+}
+
+bool checksum(long long card_number)
+{
+    int sum = 0;
+    for (int i = 0; card_number != 0; i++, card_number /= 10)
+    {
+        if (i % 2 == 0)
+           sum += card_number % 10;
+        else
+        {
+            int dig = 2 * (card_number % 10);
+            sum += dig / 10 + dig % 10;
+        }
+    }
+    return (sum % 10) ==  0;
+}
+
