d31cc1ffa73acd000b1e4d08638bab766c9f7625
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-07-26 05:57:35 -0700

    Update prst1.c

--- Week 2/prst1.c
+++ Week 2/prst1.c
@@ -1,3 +1,17 @@
 //Readability - challenge
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(void)
+{
+  string s = get_string("Text: ");
+  int num_words, num_sentences, num_letters;
+  num_words = num_sentences = num_letters = 0;
+  for (int i = 0; len = strlen(s); i < len; i++)
+  {
+      if (isalpha(s[i]))
+         num_letters++;
+      if ((i == 0 && s[i] != ' ') || //..
