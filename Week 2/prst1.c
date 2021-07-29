56ec95aa3eed3310deff23a631f3f9ab3fbccd57
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-07-29 05:50:15 -0700

    Update prst1.c

--- Week 2/prst1.c
+++ Week 2/prst1.c
@@ -1,4 +1,3 @@
-//Readability - challenge
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
@@ -7,11 +6,21 @@
 
 int main(void)
 {
-  string s = get_string("Text: ");
-  int num_words, num_sentences, num_letters;
-  num_words = num_sentences = num_letters = 0;
-  for (int i = 0; len = strlen(s); i < len; i++)
-  {
-      if (isalpha(s[i]))
-         num_letters++;
-      if ((i == 0 && s[i] != ' ') || //..
+    string s = get_string("Text: ");
+    int num_words, num_sentence, num_letter;
+    num_words = num_sentence = num_letter = 0;
+    for (int i = 0, len = strlen(s); i < len; i++)
+    {
+        if (isalpha(s[i]))
+           num_letter++;
+        if ((i == 0 && s[i] != ' ') || (i != len - 1 && s[i] == ' ' && s[i + 1] != ' '))
+           num_words++;
+        if (s[i] == '.' || s[i] == '?' || s[i] == '!')
+           num_sentence++;
+    }
+    float L = (num_letter / (float)num_words) * 100;
+    float S = (num_sentence / (float)num_words) * 100;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    printf("Grade %i\n", index);
+}
