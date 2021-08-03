95e9aa80a2652c97945a807539c650511d4f0ea4
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-03 03:20:42 -0700

    Update scrabble.c

--- Week 2/scrabble.c
+++ Week 2/scrabble.c
@@ -1,19 +1,24 @@
-//Amoo-Srwsh
 #include <stdio.h>
 #include <cs50.h>
-#include <string.h>
 #include <ctype.h>
+
 
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
+
+int string_len(string s)
+{
+    int i = 0;
+    for (; s[i] != '\0'; i++);
+    return i;
+}
 
 int main(void)
 {
     // Get input words from both players
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
-
 
     // Score both words
     int score1 = compute_score(word1);
@@ -39,12 +44,12 @@ int compute_score(string word)
 {
     int sum = 0;
 
-    for (int i = 0; i < strlen(word); i++)
+    for (int i = 0; i < string_len(word); i++)
     {
         if (isalpha(word[i]))
         {
-        int letter_number = toupper(word[i]) - 65;
-        sum += POINTS[letter_number];
+            int letter_number = toupper(word[i]) - 65;
+            sum += POINTS[letter_number];
         }
     }
 
