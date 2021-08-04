8efc0493afc7d8f7c9929b752095ff7a5de29221
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-04 15:19:14 -0700

    Update prst2.c

--- Week 2/prst2.c
+++ Week 2/prst2.c
@@ -1,4 +1,23 @@
-//Caesar
 #include <stdio.h>
-#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
+
+bool check_valid (string s)
+{
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (isalpha(s[i]))
+            return false;
+    }
+    return true;
+}
+
+int main(int argc, string argv[])
+{
+    if (argc != 2 || !check_valid(srgv[1]))
+    {
+        printf("Usage: ./caesar key\n")
+        return 1;
+    }
+}
