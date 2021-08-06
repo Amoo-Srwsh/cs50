246b65bc616cdb2c28918a95b8f2858821e5c387
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-06 05:48:43 -0700

    Update prst2.c

--- Week 2/prst2.c
+++ Week 2/prst2.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
-bool check_valid (string s)
+bool check_valid (char* s)
 {
     for (int i = 0; i < strlen(s); i++)
     {
@@ -13,11 +14,25 @@ bool check_valid (string s)
     return true;
 }
 
-int main(int argc, string argv[])
+int main(int argc, char* argv[])
 {
-    if (argc != 2 || !check_valid(srgv[1]))
+    if (argc != 2 || !check_valid(argv[1]))
     {
-        printf("Usage: ./caesar key\n")
+        printf("Usage: ./caesar key\n");
         return 1;
     }
+
+    printf("plaintext:  ");
+    char str[100];
+    scanf("%s", &str);
+
+
+    int k = atoi(argv[1]);
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        //soon
+    }
+    
+    return 0;
 }
