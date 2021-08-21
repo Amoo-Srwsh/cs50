bc71f2c8df02d0b989e337e6445bdcb533cad47c
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-21 12:48:42 -0700

    Update prst2.c

--- Week 3/prst2.c
+++ Week 3/prst2.c
@@ -177,7 +177,6 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-
        for (int i = 0; i < candidate_count; i++)
        {
 		int min = candidates[i].votes;
