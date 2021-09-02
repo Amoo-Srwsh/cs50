7598eda38b84c5a86e8b920f0017d83a9aca9048
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-09-02 09:16:01 -0700

    Update volume.c

--- Week 3/prst4/lab4/volume.c
+++ Week 3/prst4/lab4/volume.c
@@ -36,9 +36,18 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // Copy header from input file to output file
+    ONE_BYTE header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
 
-    // TODO: Read samples from input file and write updated data to output file
+    // Read samples from input file and write updated data to output file
+    AUDIO_BYTE buffer;
+    while (fread(&buffer, sizeof(buffer), 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(buffer), 1, output);
+    }
 
     // Close files
     fclose(input);
