e16d082b227ff490a25d2839fbb51393584b9eca
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-09-02 09:36:08 -0700

    Update volume.c

--- Week 3/prst4/lab4/volume.c
+++ Week 3/prst4/lab4/volume.c
@@ -20,15 +20,8 @@ int main(int argc, char *argv[])
     }
 
     // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL)
-    {
-        printf("Could not open file.\n");
-        return 1;
-    }
-
-    FILE *output = fopen(argv[2], "w");
-    if (output == NULL)
+    FILE *input = fopen(argv[1], "r"), *output = fopen(argv[2], "w");
+    if (input == NULL || output == NULL)
     {
         printf("Could not open file.\n");
         return 1;
